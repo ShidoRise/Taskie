39d1b673be507d6ee0a96eebd7b15e128b3f8542
Shido Pham <huyson22092004@gmail.com>
2025-03-25 08:38:45 +0700

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Time and Task Management',
+      title: 'Taskie',
       debugShowCheckedModeBanner: false,
       color: Colors.white,
       theme: ThemeData(
